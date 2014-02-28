c412955e1f29fc6bf87475c6d3eaf7e01dfa20bb
Tyler Kellen <tyler@sleekcode.net>
2014-02-28 15:30:38 -0500

    feat!: Use liftoff instance as context for launch callback
    feat: Support split cwd and configfile locations
    feat!: Rename configLocationFlag to configPathFlag
    feat!: Support node 0.8+
    chore: Various refactors

--- test/index.js
+++ test/index.js
@@ -1,133 +1,138 @@
-const path = require('path');
-const test = require('tap').test;
 const Liftoff = require('../');
+const path = require('path');
+const expect = require('chai').expect;
 
+const NAME = 'mocha';
 
-const NAME = 'tap';
-
+var appDefaults = new Liftoff({name: NAME})
 var app = new Liftoff({
   processTitle: NAME,
   configName: NAME+'file',
-  moduleName: NAME
+  moduleName: NAME,
+  cwdFlag: 'cwd',
+  configPathFlag: NAME+'file',
+  preloadFlag: 'require',
+  completionFlag: 'completion',
+  completion: function() {},
+  addExtensions: [],
+  searchPaths: ['test/fixtures/search_path']
 });
 
-var appAddExtensions = new Liftoff({
-  processTitle: NAME,
-  configName: NAME+'file',
-  extensions: ['rc'],
-  moduleName: NAME
+describe('Liftoff', function () {
+
+  describe('findCwd', function () {
+    it('should return process.cwd if no special flags are passed', function () {
+      expect(app.findCwd(({}))).to.equal(process.cwd());
+    });
+    it('should return path from cwdFlag if supplied', function () {
+      expect(app.findCwd({cwd:'../'})).to.equal(path.resolve('../'));
+    });
+    it('should return directory of config if configPathFlag defined', function () {
+      expect(app.findCwd({mochafile:'test/fixtures/mochafile.js'})).to.equal(path.resolve('test/fixtures'));
+    });
+    it('should return path from cwdFlag if both it and configPathFlag are defined', function () {
+      expect(app.findCwd({cwd:'../',mochafile:'test/fixtures/mochafile.js'})).to.equal(path.resolve('../'));
+    });
+  });
+
+  describe('buildEnvironment', function () {
+
+    it('should attempt pre-loading local modules if they are requested', function () {
+      var called = false;
+      app.on('require', function () {
+        called = true;
+      });
+      var env = app.buildEnvironment({require:['coffee-script']});
+      expect(called).to.be.true;
+      expect(env.preload).to.deep.equal(['coffee-script']);
+    });
+
+    it('should use configName directly if it is a regex', function () {
+      var test = new Liftoff({
+        processTitle: NAME,
+        configName: /mocha/,
+        moduleName: NAME
+      });
+      expect(test.buildEnvironment().configNameRegex.toString()).to.equal('/mocha/');
+    });
+
+    it('should use configName + addExtensions + extensions on require.extensions', function () {
+      var test = new Liftoff({
+        name: NAME,
+        addExtensions: ['rc']
+      });
+      expect(test.buildEnvironment().configNameRegex.toString()).to.equal('mochafile{rc,.js,.json,.node}');
+    });
+
+    it('should locate config using cwd', function () {
+      var cwdSaved = process.cwd();
+      process.chdir('test/fixtures');
+      expect(app.buildEnvironment({}).configPath).to.equal(path.resolve('mochafile.js'));
+      process.chdir(cwdSaved);
+    });
+
+    it('should locate config using cwdFlag if supplied', function () {
+      expect(app.buildEnvironment({cwd:'test/fixtures'}).configPath).to.equal(path.resolve('test/fixtures/mochafile.js'));
+    });
+
+    it('should return path of provided config, only if it exists', function () {
+      var configPath = path.resolve('test/fixtures/mochafile.js');
+      expect(app.buildEnvironment({mochafile:configPath}).configPath).to.equal(configPath);
+      expect(app.buildEnvironment({mochafile:'path/to/nowhere'}).configPath).to.equal(null);
+    });
+
+    it('should return path of config if found in search paths', function () {
+      expect(app.buildEnvironment().configPath).to.equal(path.resolve('test/fixtures/search_path/mochafile.js'));
+    });
+
+    it('should find module in the directory next to config', function () {
+      expect(app.buildEnvironment().modulePath).to.equal(path.resolve('node_modules/mocha/index.js'));
+    });
+
+    it('should require the package sibling to the module', function () {
+      expect(app.buildEnvironment().modulePackage).to.equal(require('../node_modules/mocha/package.json'));
+    });
+
+  });
+
+  describe('launch', function () {
+
+    it('should return early if completions are available and requested', function (done) {
+      var called = false;
+      var test = new Liftoff({
+        name: 'whatever',
+        completions: function () {
+          done();
+        }
+      });
+      test.launch(function(){},{completion:true});
+    });
+
+    it('should set the process.title to the moduleName', function () {
+      app.launch(function(){});
+      expect(process.title).to.equal(app.moduleName);
+    });
+
+    it('should call launch with liftoff instance as context', function (done) {
+      app.launch(function () {
+        expect(this).to.equal(app);
+        done();
+      });
+    });
+
+    it('should pass environment to first argument of launch callback', function (done) {
+      process.argv = [];
+      app.launch( function (env) {
+        expect(env).to.deep.equal(app.buildEnvironment({"_":[]}));
+        done();
+      });
+    });
+  });
+
 });
 
+require('./file_search');
+require('./parse_options');
+require('./silent_require');
+require('./valid_extensions');
 
-test('constructor', function (t) {
-
-  test('the "name" option auto-configures processTitle, moduleName, configFile & configLocationFlag', function (t) {
-    var alt = new Liftoff({name:NAME});
-    t.equal(alt.processTitle, NAME);
-    t.equal(alt.configName, NAME+'file');
-    t.deepEqual(alt.moduleName, NAME);
-    t.deepEqual(alt.configLocationFlag, NAME+'file');
-    t.end();
-  });
-
-  test('configures a title to be used for the process at launch', function (t) {
-    t.equal(app.processTitle, NAME);
-    t.throws(function () {
-      new Liftoff();
-    }, new Error('You must specify a processTitle.'), 'throws if not provided');
-    t.end();
-  });
-
-  test('configures the configuration file to look for at launch', function (t) {
-    t.equal(app.configName, NAME+'file');
-    t.throws(function () {
-      new Liftoff({processTitle:NAME});
-    }, new Error('You must specify a configName.'), 'throws if not provided');
-    t.end();
-  });
-
-  test('configures local module to resolve at launch', function (t) {
-    t.equal(app.moduleName, NAME);
-    t.end();
-  });
-
-  test('configures a cli flag for explicitly specifying a config location', function (t) {
-    var alt = new Liftoff({
-      name: NAME,
-      configLocationFlag: 'alt'
-    });
-    t.equal(app.configLocationFlag, NAME+'file', 'defaults to "configName+file"');
-    t.equal(alt.configLocationFlag, 'alt');
-    t.end();
-  });
-
-  test('configures a cli flag to support changing the cwd', function (t) {
-    var alt = new Liftoff({
-      name: NAME,
-      cwdFlag: 'alt'
-    });
-    t.equal(app.cwdFlag, 'cwd', 'defaults to "cwd"');
-    t.equal(alt.cwdFlag, 'alt');
-    t.end();
-  });
-
-  test('configures a cli flag to support pre-loading modules', function (t) {
-    var alt = new Liftoff({
-      name: NAME,
-      preloadFlag: 'alt'
-    });
-    t.equal(app.preloadFlag, 'require', 'defaults to "require"');
-    t.equal(alt.preloadFlag, 'alt');
-    t.end();
-  });
-
-  test('configures a cli flag to support completions', function (t) {
-    var alt = new Liftoff({
-      name: NAME,
-      completionFlag: 'alt'
-    });
-    t.equal(app.completionFlag, 'completion', 'defaults to "completion"');
-    t.equal(alt.completionFlag, 'alt');
-    t.end();
-  });
-
-  t.end();
-});
-
-test('launch(fn)', function (t) {
-  var env_ctx = false;
-  var env_passed = false;
-  t.throws(function() {
-    app.launch();
-  }, 'throws if no callback is provided');
-
-  app.launch(function (env) {
-    env_applied = this;
-    env_passed = env;
-  }, {
-    require: ['coffee-script/register'],
-    cwd: './fixtures'
-  });
-  t.ok(env_applied, 'invokes a provided callback with environment as context');
-  t.ok(env_passed, 'invokes a provided callback with environment as argument');
-
-  var expected = {
-    settings: app,
-    argv: {
-      require: ['coffee-script/register'],
-      cwd: './fixtures'
-    },
-    cwd: path.join(__dirname, 'fixtures'),
-    preload: ['coffee-script/register'],
-    validExtensions: [ '.js', '.json', '.node', '.coffee', '.litcoffee', '.coffee.md' ],
-    configNameRegex: 'tapfile{.js,.json,.node,.coffee,.litcoffee,.coffee.md}',
-    configPath: path.join(__dirname,'fixtures','Tapfile.js'),
-    configBase: path.join(__dirname, 'fixtures'),
-    modulePackage: require('../node_modules/tap/package.json'),
-    modulePath: path.join(__dirname,'../node_modules/tap/lib/main.js')
-  };
-  t.deepEqual(env_applied, expected);
-  t.deepEqual(env_passed, expected);
-
-  t.end();
-});
