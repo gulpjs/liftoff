13f014ae7aad066262a82ceccb18d366f49a85d6
Blaine Bublitz <blaine.bublitz@gmail.com>
2019-04-25 15:55:04 +0200

    remove extra tests and reword the config tests

--- test/index.js
+++ test/index.js
@@ -732,7 +732,7 @@ describe('Liftoff', function () {
   });
 
   describe('config', function () {
-    it('should be empty if not specified', function (done) {
+    it('is empty if `configFiles` is not specified', function (done) {
       var app = new Liftoff({
         name: 'myapp',
       });
@@ -742,7 +742,7 @@ describe('Liftoff', function () {
       });
     });
 
-    it('TODO: name', function (done) {
+    it('loads config if a `configFiles` is found and makes it available with the same key on `config`', function (done) {
       var app = new Liftoff({
         name: 'myapp',
         configFiles: {
@@ -759,7 +759,7 @@ describe('Liftoff', function () {
       });
     });
 
-    it('TODO: name with extends', function (done) {
+    it('loads and merges a config file specified if loaded file provides `extends` property', function (done) {
       var app = new Liftoff({
         name: 'myapp',
         configFiles: {
@@ -773,161 +773,6 @@ describe('Liftoff', function () {
             bbb: 'BBB',
           },
         });
-        done();
-      });
-    });
-
-    it.skip('should use default cwd if not specified', function (done) {
-      var app = new Liftoff({
-        name: 'myapp',
-        configFiles: {
-          testconfig: [
-            { path: '.', extensions: ['.js', '.json'] },
-          ],
-        },
-      });
-      app.prepare({
-        cwd: 'test/fixtures/configfiles',
-      }, function (env) {
-        expect(env.configFiles).to.deep.equal({
-          testconfig: path.resolve('./test/fixtures/configfiles/testconfig.json'),
-        });
-        done();
-      });
-    });
-
-    it.skip('should use default extensions if not specified', function (done) {
-      var app = new Liftoff({
-        extensions: { '.md': null, '.txt': null },
-        name: 'myapp',
-        configFiles: {
-          README: {
-            markdown: {
-              path: '.',
-            },
-            text: {
-              path: 'test/fixtures/configfiles',
-            },
-            markdown2: {
-              path: '.',
-              extensions: ['.json', '.js'],
-            },
-            text2: {
-              path: 'test/fixtures/configfiles',
-              extensions: ['.json', '.js'],
-            },
-          },
-        },
-      });
-      app.prepare({}, function (env) {
-        expect(env.configFiles).toEqual({
-          README: {
-            markdown: path.resolve('./README.md'),
-            text: path.resolve('./test/fixtures/configfiles/README.txt'),
-            markdown2: null,
-            text2: null,
-          },
-        });
-        done();
-      });
-    });
-
-    it.skip('should use specified loaders', function (done) {
-      var logRequire = [];
-      var logFailure = [];
-
-      var app = new Liftoff({
-        extensions: {
-          '.md': './test/fixtures/configfiles/require-md',
-        },
-        name: 'myapp',
-        configFiles: {
-          README: {
-            text_null: {
-              path: 'test/fixtures/configfiles',
-            },
-            text_err: {
-              path: 'test/fixtures/configfiles',
-              extensions: {
-                '.txt': './test/fixtures/configfiles/require-non-exist',
-              },
-            },
-            text: {
-              path: 'test/fixtures/configfiles',
-              extensions: {
-                '.txt': './test/fixtures/configfiles/require-txt',
-              },
-            },
-            markdown: {
-              path: '.',
-            },
-            markdown_badext: {
-              path: '.',
-              extensions: {
-                '.txt': './test/fixtures/configfiles/require-txt',
-              },
-            },
-            markdown_badext2: {
-              path: '.',
-              extensions: {
-                '.txt': './test/fixtures/configfiles/require-non-exist',
-              },
-            },
-          },
-          // Intrinsic extension-loader mappings are prioritized.
-          index: {
-            test: {
-              path: 'test/fixtures/configfiles',
-              extensions: {
-                // ignored
-                '.js': './test/fixtures/configfiles/require-js',
-                '.json': './test/fixtures/configfiles/require-json',
-              },
-            },
-          },
-        },
-      });
-      app.on('loader:failure', function (moduleName, error) {
-        logFailure.push({ moduleName: moduleName, error: error });
-      });
-      app.on('loader:success', function (moduleName, module) {
-        logRequire.push({ moduleName: moduleName, module: module });
-      });
-      app.prepare({}, function (env) {
-        expect(env.configFiles).toEqual({
-          README: {
-            text: path.resolve('./test/fixtures/configfiles/README.txt'),
-            text_null: null,
-            text_err: path.resolve('./test/fixtures/configfiles/README.txt'),
-            markdown: path.resolve('./README.md'),
-            markdown_badext: null,
-            markdown_badext2: null,
-          },
-          index: {
-            test: path.resolve('./test/fixtures/configfiles/index.json'),
-          },
-        });
-
-        expect(logRequire.length).toEqual(2);
-        expect(logRequire[0].moduleName).toEqual(
-          './test/fixtures/configfiles/require-txt'
-        );
-        expect(logRequire[1].moduleName).toEqual(
-          './test/fixtures/configfiles/require-md'
-        );
-
-        expect(logFailure.length).toEqual(1);
-        expect(logFailure[0].moduleName).toEqual(
-          './test/fixtures/configfiles/require-non-exist'
-        );
-
-        expect(require(env.configFiles.README.markdown)).toEqual(
-          'Load README.md by require-md'
-        );
-        expect(require(env.configFiles.README.text)).toEqual(
-          'Load README.txt by require-txt'
-        );
-        expect(require(env.configFiles.index.test)).toEqual({ aaa: 'AAA' });
         done();
       });
     });
